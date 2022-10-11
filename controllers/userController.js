0d5e72ec82e9c762044bd124a9bcb0c95b09c5a6
MadeCodeRD <Madelson.Acosta2@gmail.com>
2022-10-11 18:28:48 -0400

    initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,72 @@
+const User = require('../models/User');
+const CustomError = require('../errors');
+const { StatusCodes } = require('http-status-codes');
+const { createTokenUser, attachCookiesToResponse, checkPermissions } = require('../utils');
+
+const getAllUsers = async (req, res) => {
+  const users = await User.find({ role: 'user' }).select('-password');
+  res.status(StatusCodes.OK).json({ users });
+};
+
+const getSingleUser = async (req, res) => {
+  const { id: userId } = req.params;
+  const user = await User.findOne({ _id: userId }).select('-password');
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user found with id ${userId}`);
+  }
+  checkPermissions(req.user, user._id);
+  res.status(StatusCodes.OK).json({ user });
+};
+
+const showCurrentUser = async (req, res) => {
+  res.status(StatusCodes.OK).json({ user: req.user });
+};
+
+const updateUser = async (req, res) => {
+  const { email, name } = req.body;
+  if (!email || !name) {
+    throw new CustomError.BadRequestError('Please provide all values');
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.name = name;
+  user.email = email;
+
+  await user.save();
+
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+
+const updateUserPassword = async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError(
+      'Please provide old and new passwords'
+    );
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+
+  user.password = newPassword;
+
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: 'SUCCESS, password updated' });
+};
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+};
